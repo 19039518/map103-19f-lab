3879aa347854c383c728cdfcbf185d5703b9aafc
19039518 <19039518@B105519.saultcollege.ca>
2019-09-17 10:27:07 -0400

    final lab2

--- src/map103lab/RandomQuizHelper.java
+++ src/map103lab/RandomQuizHelper.java
@@ -10,24 +10,41 @@ package map103lab;
  * @author 19039518
  */
 public class RandomQuizHelper {
-    private String[]questions;
-    RandomQuizHelper (){
-        
-        questions=new String[5];
-        questions[0]="What is [+]";
-        questions[1]="what is [7+1]";
-        questions[2]="what is the color of snow";
-        questions[3]="what is class code";
-        questions[4]="where are we?";
-    }
-        public String getQuestions(int questionIndex){
-            String currentQuestion="";
-             currentQuestion=Questions(questionIndex);
-             return currentQuestion;
-            
-        }
-              
-        
-    }
- 
 
+    private String[] questions = new String[5];
+    private String[] answers = new String[5];
+
+    RandomQuizHelper() {
+
+        questions = new String[5];
+        questions[0] = "What is 1+1";
+        questions[1] = "what is 7+9";
+        questions[2] = "what is the color of snow";
+        questions[3] = "what is class code";
+        questions[4] = "where are we?";
+
+        answers[0] = "2";
+        answers[1] = "16";
+        answers[2] = "white";
+        answers[3] = "103";
+        answers[4] = "sault ste. marie";
+    }
+
+    public String getQuestion(int questionIndex) {
+        String currentQuestion = "";
+        currentQuestion = questions[questionIndex];
+        return currentQuestion;
+
+    }
+
+    public boolean getResult(int questionNumber, String answer) {
+        if (answers[questionNumber].equals(answer)) {
+
+            return true;
+        } else {
+            return false;
+
+        }
+
+    }
+}
